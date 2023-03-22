43b2d6cffb6b6cf710cd727596498e738f3f8916
prantoe <suwarnopranto@gmail.com>
2023-03-22 10:26:11 +0800

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,51 @@
 <template>
-  <div class="bg-dark">
+  <div class="bg-secondary">
     <div class="container py-3">
-      <div class="align-items-center">
-        <span class="margin-left-10">© 2023 Copyright: Pranto Soearno</span>
-      </div>
+      <!-- <div class="row pt-1 align-items-center"> -->
+        <!-- <div
+          class="col-xl-6 col-bg-6 col-md-6 col-sm-12 pbelow"
+          style="color: white;"
+        >
+      </div> -->
+      <p class="text-center pbelow">© 2021 Copyright: pranto soearno</p>
+
+        
+      <!-- </div> -->
     </div>
   </div>
 </template>
+
+<script>
+
+</script>
+
+<style scoped>
+p {
+  font-weight: 500;
+  color: aliceblue;
+}
+
+.btn {
+  border-color: white;
+  color: white;
+}
+
+.btn:hover {
+  background-color: white;
+  border-color: white;
+  color: rgb(224, 220, 220);
+}
+
+.btn:focus {
+  background-color: white;
+  border-color: white;
+  color: gray;
+}
+
+@media screen and (max-width: 580px) {
+  .pbelow {
+    padding-bottom: 20px;
+    text-align: center;
+  }
+}
+</style>
