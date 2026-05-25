7fd6f50606b1bb2cf817d2d331993cee5e133714
Pranto <suwarnopranto@gmail.com>
2026-05-26 00:36:23 +0700

    fix: upgrade to Node 24, patch webpack md4 hash for OpenSSL 3 compat

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const crypto = require('crypto');
+const origCreateHash = crypto.createHash;
+crypto.createHash = (algorithm) =>
+  origCreateHash(algorithm === 'md4' ? 'sha256' : algorithm);
+
 module.exports = {
     chainWebpack: config => {
         config.module.rules.delete('eslint');
